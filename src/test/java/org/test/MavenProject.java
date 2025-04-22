33aaebdb5ea13a03d410f62c9091ebe7164570c9
roshan543 <roshanp2908@gmail.com>
2025-04-22 12:32:03 +0530

    changes made

--- src/test/java/org/test/MavenProject.java
+++ src/test/java/org/test/MavenProject.java
@@ -9,6 +9,7 @@ public class MavenProject extends BaseClass{
 btnclick(driver.findElement(By.xpath("(//a[@role='button'])[2]")));
       wait(3);
       Sample p= new Sample();
+      Sample R= new Sample();
       sendkeys(p.getfirstname(),"Praveen123");
       sendkeys(p.getlastname(),"Roshan");
       select(p.day(),"26");
